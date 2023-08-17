9d2c3b6ac56f27b60c2b9b02dc09689f4c1c6b45
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2023-08-17 11:58:39 -0400

    Start up config
    getting the server live and connected to the databases

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -5,63 +5,63 @@
  */
 
 require('dotenv').config();
-const path = require("path");
+const path = require('path');
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
-  DEBUG,
+  DATABASE_URL = 'postgres://qnwauhxr:t8DMleB9hv4paCXmEN_wIiF39-h767Bu@batyr.db.elephantsql.com/qnwauhxr',
+  DATABASE_URL_DEVELOPMENT = 'postgres://ssedubwz:J9XZ3mzzJrOt1lfq08rLnpJYaI-l0myz@batyr.db.elephantsql.com/ssedubwz',
+  DATABASE_URL_TEST = 'postgres://ycqyryik:dLs9F4ecNeo5PidssdPFmePbTnnfjzHp@batyr.db.elephantsql.com/ycqyryik',
+  DATABASE_URL_PREVIEW = 'postgres://sxoubqsi:UQNStg_gIc_hHKe0zwTx9IHkt0WcbzM5@batyr.db.elephantsql.com/sxoubqsi',
+  DEBUG
 } = process.env;
 
 module.exports = {
   development: {
-    client: "postgresql",
+    client: 'postgresql',
     pool: { min: 1, max: 5 },
     connection: DATABASE_URL_DEVELOPMENT,
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations')
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds')
     },
-    debug: !!DEBUG,
+    debug: !!DEBUG
   },
   test: {
-    client: "postgresql",
+    client: 'postgresql',
     pool: { min: 1, max: 5 },
     connection: DATABASE_URL_TEST,
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations')
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds')
     },
-    debug: !!DEBUG,
+    debug: !!DEBUG
   },
   preview: {
-    client: "postgresql",
+    client: 'postgresql',
     pool: { min: 1, max: 5 },
     connection: DATABASE_URL_PREVIEW,
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations')
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds')
     },
-    debug: !!DEBUG,
+    debug: !!DEBUG
   },
   production: {
-    client: "postgresql",
+    client: 'postgresql',
     pool: { min: 1, max: 5 },
     connection: DATABASE_URL,
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations')
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds')
     },
-    debug: !!DEBUG,
-  },
+    debug: !!DEBUG
+  }
 };
