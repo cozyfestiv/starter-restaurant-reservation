8bfdc039f8f4cc8e525aee5c17911def691ea8ef
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2023-09-04 20:58:22 -0400

    troubleshooting branch conflict

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -24,22 +24,6 @@ function read (reservation_id) {
   return knex('reservations').select('*').where({ reservation_id }).first();
 }
 
-function update (updatedReservation) {
-  return knex('reservations')
-    .select('*')
-    .where({ reservation_id: updatedReservation.reservation_id })
-    .update(updatedReservation, '*');
-}
-
-function search (mobile_number) {
-  return knex('reservations')
-    .whereRaw(
-      "translate(mobile_number, '() -', '') like ?",
-      `%${mobile_number.replace(/\D/g, '')}%`
-    )
-    .orderBy('reservation_date');
-}
-
 module.exports = {
   list,
   listByDate,
