c0b8417dad6e1d0ada780cca79f49a5ee12831db
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2023-09-04 20:57:37 -0400

    troubleshooting branch conflict

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -2,6 +2,30 @@ const service = require('./reservations.service');
 const hasProperties = require('../errors/hasProperties');
 const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
 
+async function list (req, res) {
+  const { date } = req.query;
+  if (date) {
+    res.json({ data: await service.listByDate(date) });
+  } else {
+    console.log(await service.list(), '^^^^^^^^^^^^^');
+    res.json({ data: await service.list() });
+  }
+}
+
+function read (req, res) {
+  const data = res.locals.reservation;
+  res.json({ data });
+}
+
+async function create (req, res) {
+  const newReservation = await service.create(req.body.data);
+  res.status(201).json({
+    data: newReservation[0]
+  });
+}
+
+//MIDDLEWARE
+
 const VALID_PROPERTIES = [
   'first_name',
   'last_name',
@@ -16,8 +40,10 @@ const VALID_PROPERTIES = [
 ];
 
 async function reservationExists (req, res, next) {
+  // console.log(req.params, '----------------------------');
   const { reservation_id } = req.params;
   const reservation = await service.read(reservation_id);
+  console.log(reservation, '----------------------------');
   if (reservation) {
     res.locals.reservation = reservation;
     return next();
@@ -105,32 +131,6 @@ function hasValidDateTime (req, res, next) {
   }
   next();
 }
-
-async function list (req, res) {
-  const { date, mobile_number } = req.query;
-
-  if (date) {
-    res.json({ data: await service.listByDate(date) });
-  } else if (mobile_number) {
-    res.json({ data: await service.search(mobile_number) });
-  } else {
-    res.json({ data: await service.list() });
-  }
-}
-
-function read (req, res) {
-  const { reservation } = res.locals;
-  res.json({ data: reservation });
-}
-
-async function create (req, res) {
-  const newReservation = await service.create(req.body.data);
-  res.status(201).json({
-    data: newReservation[0]
-  });
-}
-
-//MIDDLEWARE
 
 module.exports = {
   list: asyncErrorBoundary(list),
