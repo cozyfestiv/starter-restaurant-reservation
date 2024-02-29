087c21b6462731aa2b3c29a05770433c5ab3414d
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-02-29 17:03:11 -0500

    Updated Make a Reservation
    Make a reservation is now in card form
    Adjusted the inputs to look more up to date

--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -75,115 +75,137 @@ function ReservationForm () {
 
   return (
     <>
-      {reservation_id ? <h2>Edit Reservation</h2> : <h2>Make a Reservation</h2>}
       <ErrorAlert error={reservationsError} />
-      <form onSubmit={handleSubmit}>
-        <div className='container'>
-          <div className='form-group formDivCreate'>
-            <label htmlFor='first_name'>
-              First Name:
-              <input
-                className='form-control'
-                type='text'
-                id='first_name'
-                name='first_name'
-                placeholder='First Name'
-                onChange={handleChange}
-                required={true}
-                value={form.first_name}
-              />
-            </label>
+      <div className='card cards mx-5 mt-5'>
+        <form onSubmit={handleSubmit}>
+          <div className='card-header'>
+            {reservation_id ? (
+              <h2 className='heading'>Edit Reservation</h2>
+            ) : (
+              <h2 className='heading'>Make a Reservation</h2>
+            )}
           </div>
+          <div className='container card-body d-flex justify-space-evenly'>
+            <div className='d-flex flex-column flex-grow-1'>
+              <div className='form-group d-flex justify-content-center'>
+                <label htmlFor='first_name' className='w-100 text-center'>
+                  First Name:
+                  <input
+                    className='form-control'
+                    type='text'
+                    id='first_name'
+                    name='first_name'
+                    placeholder='First Name'
+                    onChange={handleChange}
+                    required={true}
+                    value={form.first_name}
+                  />
+                </label>
+              </div>
 
-          <div className='form-group formDivCreate'>
-            <label htmlFor='last_name'>
-              Last Name:
-              <input
-                className='form-control'
-                type='text'
-                id='last_name'
-                name='last_name'
-                placeholder='Last Name'
-                onChange={handleChange}
-                required={true}
-                value={form.last_name}
-              />
-            </label>
-          </div>
+              <div className='form-group d-flex justify-content-center'>
+                <label htmlFor='last_name' className='w-100 text-center'>
+                  Last Name:
+                  <input
+                    className='form-control'
+                    type='text'
+                    id='last_name'
+                    name='last_name'
+                    placeholder='Last Name'
+                    onChange={handleChange}
+                    required={true}
+                    value={form.last_name}
+                  />
+                </label>
+              </div>
 
-          <div className='form-group formDivCreate'>
-            <label htmlFor='mobile_number'>
-              Phone number:
-              <input
-                className='form-control'
-                type='tel'
-                id='telNo'
-                name='mobile_number'
-                placeholder='1234567890'
-                onChange={handleChange}
-                required={true}
-                value={form.mobile_number}
-              />
-            </label>
-          </div>
+              <div className='form-group d-flex justify-content-start'>
+                <label htmlFor='mobile_number' className='w-75 text-center'>
+                  Phone number:
+                  <input
+                    className='form-control'
+                    type='tel'
+                    id='telNo'
+                    name='mobile_number'
+                    placeholder='1234567890'
+                    onChange={handleChange}
+                    required={true}
+                    value={form.mobile_number}
+                  />
+                </label>
+              </div>
+            </div>
+            <div className='d-flex flex-column flex-grow-1'>
+              <div className='form-group d-flex justify-content-center'>
+                <label
+                  htmlFor='reservation_date'
+                  className='w-75 px-5 text-center'
+                >
+                  Reservation Date:
+                  <input
+                    className='form-control'
+                    type='date'
+                    id='reservation_date'
+                    name='reservation_date'
+                    onChange={handleChange}
+                    required={true}
+                    value={form.reservation_date.substring(0, 10)}
+                  />
+                </label>
+              </div>
 
-          <div className='form-group formDivCreate'>
-            <label htmlFor='reservation_date'>
-              Reservation Date:
-              <input
-                className='form-control'
-                type='date'
-                id='reservation_date'
-                name='reservation_date'
-                onChange={handleChange}
-                required={true}
-                value={form.reservation_date.substring(0, 10)}
-              />
-            </label>
-          </div>
+              <div className='form-group d-flex justify-content-center'>
+                <label
+                  htmlFor='reservation_time'
+                  className='w-75 px-5 text-center'
+                >
+                  Reservation Time:
+                  <input
+                    className='form-control'
+                    type='time'
+                    id='reservation_time'
+                    name='reservation_time'
+                    onChange={handleChange}
+                    required={true}
+                    value={form.reservation_time.substring(0, 5)}
+                  />
+                </label>
+              </div>
 
-          <div className='form-group formDivCreate'>
-            <label htmlFor='reservation_time'>
-              Reservation Time:
-              <input
-                className='form-control'
-                type='time'
-                id='reservation_time'
-                name='reservation_time'
-                onChange={handleChange}
-                required={true}
-                value={form.reservation_time.substring(0, 5)}
-              />
-            </label>
+              <div className='form-group d-flex justify-content-center'>
+                <label htmlFor='people' className='w-75 px-5 text-center'>
+                  Number of People in Party:
+                  <input
+                    className='form-control'
+                    type='number'
+                    id='people'
+                    name='people'
+                    min='1'
+                    required={true}
+                    onChange={handleChange}
+                    value={form.people}
+                  />
+                </label>
+              </div>
+            </div>
           </div>
-
-          <div className='form-group formDivCreate'>
-            <label htmlFor='people'>
-              Number of People in Party:
-              <input
-                className='form-control'
-                type='number'
-                id='people'
-                name='people'
-                min='1'
-                required={true}
-                onChange={handleChange}
-                value={form.people}
-              />
-            </label>
+          <div className='card-footer d-flex justify-content-center'>
+            <button
+              type='submit'
+              className='btn submitBtn font-weight-bolder mx-2'
+            >
+              Submit
+            </button>
+            <button
+              type='button'
+              className='btn cancelBtn font-weight-bolder mx-2'
+              onClick={() => history.goBack()}
+            >
+              Cancel
+            </button>
           </div>
-        </div>
-        <button type='submit' className='btn submitBtn font-weight-bolder mx-2'>
-          Submit
-        </button>
-        <button
-          type='button'
-          className='btn cancelBtn font-weight-bolder mx-2'
-          onClick={() => history.goBack()}
-        >
-          Cancel
-        </button>
-      </form>
+        </form>
+      </div>
     </>
   );
 }
